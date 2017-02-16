ad7e378e435b126eb82c8c88b0c9d3701fbca1a0
Emily Lepert <emilylepert@Users-MacBook-Pro.local>
2017-02-16 14:54:45 -0500

    Commit test

---
+++ text_mining.py
@@ -0,0 +1,8 @@
+
+import requests
+#oliver_twist_full_text = requests.get('http://www.gutenberg.org/ebooks/730.txt.utf-8').text
+#print(oliver_twist_full_text[100:])
+
+f = open('consumer_key.txt')
+print(f.read().strip())
+
