c45b24daeb83f4b63e16ab546f963076cc11db50
Matt Brucker <mpbrucker@gmail.com>
2017-02-19 22:52:31 -0500

    Added more parameters, made the title creation actually work.

--- spotify_scrape.py
+++ spotify_scrape.py
@@ -7,7 +7,6 @@ then use Markov chains to create random song names of a certain genre.
 """
 
 import argparse
-import pickle
 from spotipy import Spotify, oauth2
 import markovify
 import os.path
@@ -15,60 +14,73 @@ import os.path
 parser = argparse.ArgumentParser(description='Enter API Credentials')
 parser.add_argument('--id', help='Your client ID.')
 parser.add_argument('--secret', help='Your client secret.')
+parser.add_argument('genre', help='The genre of song to generate.')
 args = parser.parse_args()
-
-token = oauth2.SpotifyClientCredentials(client_id=args.id, client_secret=args.secret)
-sp = Spotify(client_credentials_manager=token)
-
-
-def save_all_playlists():
-    spotify_lists = sp.user_playlists('spotify')
-    all_lists = list()
-    while spotify_lists['next']:
-        all_lists.extend(spotify_lists['items'])
-        spotify_lists = sp.next(spotify_lists)
-    print(all_lists[0])
-    pickle.dump(all_lists, open('playlist_data.pickle', 'wb'))
 
 
 def save_track_list(genre):
+    """
+    Builds a pickle file containing the song names of up to 10,000 songs of a certain genre.
+    Strips extra words after hyphens from track names, and also converts everything to lowercase.
+    """
     genre_split = '+'.join(genre.split())  # Converts genre name into API-compatible string
     tracks = sp.search("genre:" + genre_split, type='track', limit=50)
-    all_tracks = list()
-    total_tracks = 0
-    # Iterate until there's no more tracks, or we hit 10,000 tracks
-    while tracks['tracks']['next'] and total_tracks < 10000:
-        for track in tracks['tracks']['items']:
-            track_name = track['name'].lower()  # Keep everything lowercase for simplicity
+    if len(tracks['tracks']['items']) == 0:  # If there were no results for this genre, raise an exception
+        raise ValueError
+    else:
+        all_tracks = list()
+        total_tracks = 0
+        punctuation = '&()\"[]:'  # Used to remove random punctuation from songs
+        # Iterate until there's no more tracks, or we hit 10,000 tracks
+        while tracks['tracks']['next'] and total_tracks < 10000:
+            for track in tracks['tracks']['items']:
+                track_name = ''.join([char for char in track['name'].lower() if char not in punctuation])  # Keep everything lowercase for simplicity
+                #  cuts off at hyphens - this is because a lot of tracks have "live" in their name, and we don't want that
+                cutoff_index = track_name.find('-')
+                if cutoff_index >= 0:
+                    track_name = track_name[:cutoff_index]
 
-            #  cuts off at hyphens - this is because a lot of tracks have "live" in their name, and we don't want that
-            cutoff_index = track_name.find('-')
-            if cutoff_index >= 0:
-                track_name = track_name[:cutoff_index]
+                # Add track name to our list of track names
+                all_tracks.append(track_name)
+            # Iterate to the next tracks
+            tracks = sp.next(tracks['tracks'])
+            total_tracks += 50
 
-            # Add track name to our list of track names
-            all_tracks.append(track_name)
-        # Iterate to the next tracks
-        tracks = sp.next(tracks['tracks'])
-        total_tracks += 50
-    # Pickles the data to reduce the number of API calls
-    pickle_name = '_'.join(genre.lower().split()) + '.pickle'
-    pickle.dump(all_tracks, open(pickle_name, 'wb'))
-
-
-def get_all_tracks(genre):
-    file_name = '_'.join(genre.lower().split()) + '.pickle'
-    if not os.path.isfile(file_name):
-        save_track_list(genre)
-    tracks = pickle.load(open(file_name, 'rb'))
-    tracks_combined = '. '.join(tracks)
-    text_model = markovify.Text(tracks_combined)
+        # Stores the data to reduce the number of API calls
+        file_name = 'tracks/{}.txt'.format('_'.join(genre.lower().split()))
+        with open(file_name, 'w') as in_file:
+            in_file.write('\n'.join(all_tracks))
 
 
+def gen_random_track(genre):
+    """
+    Generates a random track of a given genre using Markov chains.
+    """
+    # Build the name of the text file to pull from
+    file_name = 'tracks/{}.txt'.format('_'.join(genre.lower().split()))
+    try:  # Exception handling in case the genre isn't founc
+        if not os.path.isfile(file_name):
+            save_track_list(genre)  # Only call the API if the tracks file doesn't already exist
+        with open(file_name, 'r') as in_file:
+            tracks = in_file.read()  # Read the text file containing the tracks
+        # Build a Markov chain from the text file
+        text_model = markovify.NewlineText(tracks, state_size=1)
+        # Create new randomly-generated sentence and convert to uppercase
+        sentence = ' '.join([word[0].upper() + word[1:] for word in text_model.make_short_sentence(140).split()])
+        print(sentence)
+    except ValueError:
+        print('Could not find any tracks from that genre.  Try again.')
 
 
 if __name__ == '__main__':
-    # save_all_playlists()
-    # get_all_playlists()
-    # get_new_releases()
-    get_all_tracks("Indie Rock")
+    # Handle Spotify client authorization
+    if args.id and args.secret:
+        token = oauth2.SpotifyClientCredentials(client_id=args.id, client_secret=args.secret)
+    else:
+        in_file = open('creds.txt', 'r').read().split()
+        client_id = in_file[0]
+        client_secret = in_file[1]
+        token = oauth2.SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    sp = Spotify(client_credentials_manager=token)
+
+    gen_random_track(args.genre)
