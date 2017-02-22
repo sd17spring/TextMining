98642ee87a4a502af916bbca4725704ad641dc28
Ben Ziemann <benjamin.ziemann@students.olin.edu>
2017-02-21 21:18:11 -0500

    Plotly updates

--- analyze_text.py
+++ analyze_text.py
@@ -14,6 +14,16 @@ import pickle
 from bs4 import BeautifulSoup
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import re
+import plotly
+
+#Get plotly api key
+keyFile = open('plotlyAPIKey.txt','r')
+plotlyAPIKey = keyFile.read().strip()
+keyFile.close()
+plotly.tools.set_credentials_file(username='zneb97', api_key=plotlyAPIKey)
+
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 def collect_texts(page):
     """
@@ -43,10 +53,44 @@ def analyze_text():
     """
     Analayze texts for sentiment
     """
+    scores = []
+    names = []
+    poss =[]
+    negs =[]
     for filename in os.listdir("/home/zneb/SoftDes/TextMining/Texts"):
             text = open("Texts/"+filename,'r')
-            speech = text.read()
-            text.close()
+            vs = analyzer.polarity_scores(text.read())
+            name = filename.replace(".txt","")
+            names.append(name)
+            poss.append(vs['pos'])
+            negs.append(vs['neg'])
+    #         scores.append((name,"Positive: " + str(vs['pos']), "Negative: " + str(vs['neg'])))
+    #         text.close()
+    # scores.sort(key=lambda x: x[0])
+    # for go in scores:
+    #     print(go)
+    scores = [names,poss,negs]
+    return scores
+
+def graph_scores(scores):
+    trace1 = go.Bar(
+        x=scores[0],
+        y=scores[1],
+        name='Positive Score'
+    )
+    trace2 = go.Bar(
+        x=scores[0],
+        y=scores[2],
+        name='Negative Score'
+    )
+
+    data = [trace1, trace2]
+    layout = go.Layout(
+        barmode='group'
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+    py.image.save_as(fig, filename='scores.png')
 
 
 if __name__ == '__main__':
@@ -55,4 +99,5 @@ if __name__ == '__main__':
     url = "http://avalon.law.yale.edu/subject_menus/inaug.asp"
     #Collect texts
     collect_texts(url)
-    analyze_text()
+    scores = analyze_text()
+    graph_scores(scores)
