4cf46d7f59ec7ae9b06aec40b81af87d73a6ab1e
Yichen Jiang <pauljyc0929@gmail.com>
2017-02-22 14:16:59 -0500

    Turn in Project 3

--- analyzer.py
+++ analyzer.py
@@ -4,20 +4,6 @@ import random
 
 markov = dict()
 
-def remove_stuff(statuses):
-    '''
-    Remove the urls and hashtags that sometimes appear at the end of the
-    status messages
-    Returns a list of status texts
-    '''
-    res = []
-    for status in statuses:
-        cur = status.text
-        while cur.find('https:')>0:
-            cur = cur[:cur.find('https:')]
-        res.append(cur)
-    return res
-
 def generate_markov(messages,num=2):
     '''
     Process the input message and generate a markov map for the prefix and surfix.
@@ -39,26 +25,28 @@ def generate_markov(messages,num=2):
                     markov[current_words].append(word)
                 current_words = current_words[1:]+ (word,)
 
-def generate_random():
+def generate_random(a,b):
     '''
     Generate a random tweet based on the markov maping.
     '''
     global markov
     #current_words = random.choice(list(markov.keys()))
-    current_words = ('i','think')
-    tweet = current_words[0] + ' '+ current_words[1]
+    current_words = (a,b)
+    tweet = (current_words[0] + ' '+ current_words[1]).capitalize()
     while markov.get(current_words,0)!= 0:
         random_word = random.choice(markov[current_words])
-        tweet = tweet + ' ' + random_word
         current_words = current_words[1:]+ (random_word,)
+        if tweet[-1]=='.' or tweet[-1]=='!' or tweet[-1]=='?' or random_word=='i':
+            tweet = tweet + ' ' + random_word.capitalize()
+        else:
+            tweet = tweet + ' ' + random_word
     return tweet
 
 d = open('data.txt','rb')
-statuses = pickle.load(d)
+messages = pickle.load(d)
 d.close
-messages = remove_stuff(statuses)
 generate_markov(messages)
 num_times = 5
 for i in range(num_times):
-    tweet = generate_random()
+    tweet = generate_random('i','think')
     print(tweet)
--- collector.py
+++ collector.py
@@ -6,7 +6,28 @@ api = twitter.Api(consumer_key='',
                   access_token_key='',
                   access_token_secret='')
 
-statuses = api.GetUserTimeline(screen_name='ConanOBrien',count=10000)
+def remove_stuff(statuses):
+    '''
+    Remove the urls and hashtags that sometimes appear at the end of the
+    status messages
+    Returns a list of status texts
+    '''
+    res = []
+    for status in statuses:
+        cur = status.text
+        while cur.find('https:')>0:
+            cur = cur[:cur.find('https:')]
+        res.append(cur)
+    return res
+
 d = open('data.txt','wb')
-pickle.dump(statuses,d)
+statuses = api.GetUserTimeline(screen_name='ConanOBrien',count=10000)
+part1 = remove_stuff(statuses)
+statuses = api.GetUserTimeline(screen_name='kevinseccia',count=10000)
+part2 = remove_stuff(statuses)
+statuses = api.GetUserTimeline(screen_name='weedguy420boner',count=10000)
+part3 = remove_stuff(statuses)
+part1.extend(part2)
+part1.extend(part3)
+pickle.dump(part1,d)
 d.close()
