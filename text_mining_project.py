e3f7262669283829093519d86ddacadcf05e5241
hkolano <hannah.kolano@students.olin.edu>
2017-02-22 16:31:21 -0500

    creates dictionary of prefixes and suffixes

--- text_mining_project.py
+++ text_mining_project.py
@@ -10,8 +10,8 @@ input_file2 = open('aenid_text.pickle', 'rb')
 aenid_reloaded = pickle.load(input_file2)
 aenid_reloaded = aenid_reloaded[545:-19100]
 
-text = 'Hello cruel world \n Why are people cruel \n I do not love the world'
-prefix_length = 2
+text = 'Hello cruel world \n Why are people cruel \n I do not love the world \n Why are we here'
+prefix_length = 1
 
 
 def process_file(text):
@@ -19,14 +19,14 @@ def process_file(text):
     prefixes_dict = dict()
     line_hangover = []
     for line in text.splitlines():
-        line_hangover = process_line(line, hist, line_hangover)
+        line_hangover = process_line(line, hist, line_hangover, prefixes_dict)
+    print(prefixes_dict)
     return hist
 
 
-def process_line(line, hist, line_hangover):
+def process_line(line, hist, extended_line, prefixes_dict):
     line = line.replace('-', ' ')
     line = line.replace("'", '')
-    extended_line = line_hangover
     for word in line.split():
         word = word.strip(string.punctuation + string.whitespace)
         word = word.lower()
@@ -34,12 +34,18 @@ def process_line(line, hist, line_hangover):
             if word[0] not in string.digits:
                 hist[word] = hist.get(word, 0) + 1
                 extended_line.append(word)
-    print(extended_line)
-    return extended_line[-prefix_length:]
+    process_prefixes(extended_line, prefixes_dict)
+    line_hangover = extended_line[-prefix_length:]
+    return line_hangover
 
 
-def process_prefixes(extended_line):
-    pass
+def process_prefixes(extended_line, prefixes_dict):
+    count = 0
+    while count < len(extended_line)-prefix_length-1:
+        prefix = tuple(extended_line[count:count+prefix_length])
+        suffix = extended_line[count+prefix_length],
+        prefixes_dict[prefix] = prefixes_dict.get(prefix, tuple()) + (suffix)
+        count += 1
 
 
 def different_words(hist):
