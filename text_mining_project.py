f563350c6c79ff196e59ff2aa5c677ff18485d4c
hkolano <hannah.kolano@students.olin.edu>
2017-02-22 15:21:22 -0500

    extended lines to start prefix analysis

--- text_mining_project.py
+++ text_mining_project.py
@@ -10,22 +10,36 @@ input_file2 = open('aenid_text.pickle', 'rb')
 aenid_reloaded = pickle.load(input_file2)
 aenid_reloaded = aenid_reloaded[545:-19100]
 
+text = 'Hello cruel world \n Why are people cruel \n I do not love the world'
+prefix_length = 2
+
 
 def process_file(text):
     hist = dict()
-    text = text
+    prefixes_dict = dict()
+    line_hangover = []
     for line in text.splitlines():
-        process_line(line, hist)
+        line_hangover = process_line(line, hist, line_hangover)
     return hist
 
 
-def process_line(line, hist):
+def process_line(line, hist, line_hangover):
     line = line.replace('-', ' ')
     line = line.replace("'", '')
+    extended_line = line_hangover
     for word in line.split():
         word = word.strip(string.punctuation + string.whitespace)
         word = word.lower()
-        hist[word] = hist.get(word, 0) + 1
+        if len(word) > 0:
+            if word[0] not in string.digits:
+                hist[word] = hist.get(word, 0) + 1
+                extended_line.append(word)
+    print(extended_line)
+    return extended_line[-prefix_length:]
+
+
+def process_prefixes(extended_line):
+    pass
 
 
 def different_words(hist):
@@ -40,5 +54,4 @@ def most_common(hist):
     return t
 
 
-hist = process_file(beowulf_reloaded)
-print(most_common(hist))
+hist = process_file(text)
