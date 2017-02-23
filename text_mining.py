ea43fc587dd530e3a575e286c63e119fd1f6d159
Ben Ziemann <benjamin.ziemann@students.olin.edu>
2017-02-22 23:25:26 -0500

    Final turn in

--- text_mining.py
+++ text_mining.py
@@ -35,40 +35,51 @@ def collect_texts(page):
         #Texts are all linked from a central page
         html = BeautifulSoup(requests.get(page).text, 'lxml')
         text = str(html)
-        text = text[2200:8100]
+        text = text[2200:8150]
         #Because links are broken up into avalon base and the particular link must get in two parts
         links = re.findall(r'href="(.*?)asp',text)
-
-        for link in links:
-            rl = "http://avalon.law.yale.edu" + link[2:] + "asp"
+        #Get dates for later sorting use
+        dates = re.findall(r'">(.*?)</a>',text)
+        for i in range(len(links)):
+            url = "http://avalon.law.yale.edu" + links[i][2:] + "asp"
             name = re.findall(r'century/(.*?).asp',url)
             speech = BeautifulSoup(requests.get(url).text, 'lxml')
             speech = str(speech.get_text())
-            speech = speech[350:-470]
+            speech = speech[360:-470]
             f = open("Texts/" + name[0] + '.txt','w+')
-            f.write(speech)
+            f.write(dates[i] + " " + speech)
             f.close()
 
 def analyze_text():
     """
     Analayze texts for sentiment
     """
+    data = []
     scores = []
     names = []
+    dates = []
     poss =[]
     negs =[]
     for filename in os.listdir("/home/zneb/SoftDes/TextMining/Texts"):
             text = open("Texts/"+filename,'r')
+            date = text.read()[0:4]
+            text.seek(0) #Resets the reading
             vs = analyzer.polarity_scores(text.read())
-            name = filename.replace(".txt","")
-            names.append(name)
-            poss.append(vs['pos'])
-            negs.append(vs['neg'])
-    #         scores.append((name,"Positive: " + str(vs['pos']), "Negative: " + str(vs['neg'])))
-    #         text.close()
-    # scores.sort(key=lambda x: x[0])
-    # for go in scores:
-    #     print(go)
+            if not vs['pos'] == 0:
+                name = filename.replace(".txt","")
+                data.append((name,date,vs['pos'],vs['neg']))
+    #Various data sorts. Uncomment as needed
+    #data.sort(key=lambda x: x[0])  #Sort alphabetically by name
+    data.sort(key=lambda x: x[1])  #Sort date
+    #data.sort(key=lambda x: x[2])  #Sort by positivity
+    #data.sort(key=lambda x: x[3])  #Sort by negativity
+
+    #Break into seperate arrays to be used in graph
+    for tup in data:
+        names.append(tup[0])
+        dates.append(tup[1])
+        poss.append(tup[2])
+        negs.append(tup[3])
     scores = [names,poss,negs]
     return scores
 
@@ -86,7 +97,26 @@ def graph_scores(scores):
 
     data = [trace1, trace2]
     layout = go.Layout(
-        barmode='group'
+        barmode='group',
+        width = 1000,
+        title='Sentiment of Inaugural Speeches',
+        xaxis=dict(
+        nticks = 60,
+        title='President',
+        titlefont=dict(
+            family='Courier New, monospace',
+            size=18,
+            color='#000000'
+        )
+    ),
+    yaxis=dict(
+        title='Value',
+        titlefont=dict(
+            family='Courier New, monospace',
+            size=18,
+            color='#000000'
+        )
+    )
     )
 
     fig = go.Figure(data=data, layout=layout)
@@ -97,7 +127,6 @@ if __name__ == '__main__':
     analyzer = SentimentIntensityAnalyzer()
     #Base page
     url = "http://avalon.law.yale.edu/subject_menus/inaug.asp"
-    #Collect texts
     collect_texts(url)
     scores = analyze_text()
     graph_scores(scores)
