caf11bf8ef9268707f026e30417c2f7cd271e5eb
jsofdes <Juanita.Desouza@students.olin.edu>
2017-02-25 00:31:35 -0500

    Done. Took me today to debug because of a silly mistake yesterday.

--- pygmalionanalysis.py
+++ pygmalionanalysis.py
@@ -3,28 +3,28 @@ import pickle
 import math
 import io
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+#tested setup by printing requests
 def setup():
     import requests
     shaw_full_text = requests.get('http://www.gutenberg.org/cache/epub/3825/pg3825.txt').text
-    # print(shaw_full_text)
     import pickle
     # Save data to a file (will be part of your data fetching script)
     f = open('shaw_texts.pickle', 'wb')
     pickle.dump(shaw_full_text, f)
     f.close()
 
-
+#tested this by printing copy of texts
 def setup_functionality():
+
     import pickle
     input_file = open('shaw_texts.pickle', 'rb')
     copy_of_texts_from_pickel = pickle.load(input_file)
-    # print(copy_of_texts_from_pickel)
-    # blob= open('copy_of_texts').readline()
     Higgins_lines = []
     Liza_lines = []
     Freddy_lines = []
     play_lines = copy_of_texts_from_pickel.split('\r\n\r\n')
-    #print(play_lines)
+    #used a print of play_lines to check if this was mostly working. It was but there are some errors that are minor.
+    #if I had more time I would fix those.
     for line in play_lines:
         if line[0:7] == "HIGGINS":
             Higgins_lines.append(line)
@@ -34,19 +34,11 @@ def setup_functionality():
             Freddy_lines.append(line)
     a = [Higgins_lines, Liza_lines, Freddy_lines]
     return a
-    # return Higgins_lines
-    # return Liza_lines
-    # return Freddy_lines
-    # print(Liza_lines)
-    # print(Freddy_lines)
-    # print(Higgins_lines)
+#returned lines ot make sure they individually worked.
 
 def Sentinment_Anlyis(someone_lines):
-    # someone_lines = ['bob is good', 'bob is bad']
-    # print('SOMEONES LINES')
-    #print(someone_lines)
+    #had to test if array was working.
     analyzer = SentimentIntensityAnalyzer()
-    #total_score = []
     someone_neu=[]
     someone_pos=[]
     someone_neg=[]
@@ -57,34 +49,22 @@ def Sentinment_Anlyis(someone_lines):
         someone_pos.append(scores['pos'])
         someone_neg.append(scores['neg'])
         someone_cmpd.append(scores['compound'])
-
-
-        #print(Freddy_neu)
-        #scores['pos']
-        #scores['neg']
-        #scores['compound']
-        #total_score.append(scores)
-    #print(total_score)
+        #made sure this was working by printing these
     someone_neu_avg = sum(someone_neu)/len(someone_neu)
     someone_pos_avg = sum(someone_pos)/len(someone_pos)
     someone_neg_avg = sum(someone_neg)/len(someone_neg)
     someone_cmpd_avg = sum(someone_cmpd)/len(someone_cmpd)
     someone_avg_all = [someone_neu_avg, someone_pos_avg, someone_neg_avg, someone_cmpd_avg]
     print(someone_avg_all)
-#print(Freddy_neu_avg)
-#     b = Higginsscrip\
-# lines=[]
-# for line in blob:
-#     lines.append(line)
-# print(lines)
-
-# buf = io.StringIO(copy_of_texts_from_pickel)
 
 def analyze_all_lines():
+    #Line below can be commented/uncommented based on if you have the text alread.
+    #This is is to limit the number of requests.
+    #originally I had but this in another file
     #setup()
+    #a is the array output of higgins, liza, Freddy in that order
+    #also I'm kind of unsur eabout how/where to use docstrings so I checked via printing? Esp since the text was so long.
     a = setup_functionality()
-    #b=a[2]
-    # print(b)
     Sentinment_Anlyis(a[0])
     Sentinment_Anlyis(a[1])
     Sentinment_Anlyis(a[2])
