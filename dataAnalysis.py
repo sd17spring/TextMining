a6c0be1bf2b9f339ea10e83bddfd0a7ff1349997
aliolin <alexander.li@students.olin.edu>
2017-04-23 21:28:01 -0400

    added more websites, added sentiment analysis for MP5

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,16 +1,28 @@
 import pickle
 from bs4 import BeautifulSoup
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+def analyzer(filename):
+    input_file = open(filename, 'rb')
+    websitee = pickle.load(input_file)
+
+    soup = BeautifulSoup(websitee, "html.parser")
+
+    text = soup.get_text()
+    count = 0
+    newtext = text.split()
+    for i in newtext:
+        if(i == "trump" or i == "Trump"):
+            count += 1
+    analyze = SentimentIntensityAnalyzer()
+    scores = analyze.polarity_scores(text)
+    return count, scores
 
 
-input_file = open('cnn.pickle', 'rb')
-cnnwebsite = pickle.load(input_file)
-
-soup = BeautifulSoup(cnnwebsite, "html.parser")
-
-text = soup.get_text()
-count = 0
-text = text.split()
-for i in text:
-    if(i == "trump" or i == "Trump" or i == ""):
-        count += 1
-print(count)
+if __name__ == "__main__":
+    websites = ['bbc.pickle', 'cnn.pickle', 'foxnews.pickle']
+    for i in websites:
+        count = analyzer(i)[0]
+        scores = analyzer(i)[1]
+        print("Number of times Trump is mentioned: " , count)
+        print("Positivity/Negativity score: ", scores)
